456833c3bbf63190c8d26542a9e015095c45333b
Tambi <tambi@takingroot.org>
2025-06-05 10:17:52 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import Search from "./components/ui/Search";
 
 export default function App() {
   const [items, setItems] = React.useState([]);
-  const [page, setPage] = React.useState(2);
+  const [page, setPage] = React.useState(1);
   const [query, setQuery] = React.useState("");
   const [isLoading, setIsLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
 
   const REQUEST_URL =
     query !== ""
-      ? `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${process.env.API_KEY}&include_adult=false&language=en-US&page=${page}`
-      : `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&with_networks=213&page=${page}`;
+      ? `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=95ec914d7a53a26b5d95bd29bdafd04195ec914d7a53a26b5d95bd29bdafd041Z&include_adult=false&language=en-US&page=${page}`
+      : `https://api.themoviedb.org/3/discover/movie?api_key=95ec914d7a53a26b5d95bd29bdafd04195ec914d7a53a26b5d95bd29bdafd041Z&with_networks=213&page=${page}`;
 
   React.useEffect(() => {
     const fetchItems = async () => {
